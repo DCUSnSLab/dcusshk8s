5d371a8cce9bdf7a7cb0d80d7d249ed6fd4a90a2
chan-a12 <ami159@cu.ac.kr>
2026-05-06 20:02:47 +0900

    update image tag

--- kubessh/pod.py
+++ kubessh/pod.py
@@ -58,7 +58,7 @@ class UserPod(LoggingConfigurable):
                 "initContainers": [
                     {
                         "name": "init-setup",
-                        "image": "harbor.cu.ac.kr/k8s_dynamic_allocator/user_pod:rename",
+                        "image": "harbor.cu.ac.kr/k8s_dynamic_allocator/user_pod:latest",
                         # "image": "harbor.cu.ac.kr/swlabpods/dbuntu:latest",
                         "imagePullPolicy": "Always",
                         "command": ["/bin/bash","-c"],
